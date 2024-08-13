3a0ee9c0a81202777deb757ed6962b94273ae1ca
PayF123 <141803727+royalarijeet1@users.noreply.github.com>
2024-08-13 13:07:35 +0530

    entity, repostory and insert data through hibernate

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.entity.UserEntity;
+import com.repository.UserRepository;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/newuser")
+	public String newUser() {
+		return "NewUser";
+	}
+
+	@PostMapping("/saveuser")
+	public String saveUser(UserEntity user) {
+		System.out.println(user.getEmail());
+		// db insert
+		//dao -> insert ->query -> 
+		userRepository.save(user);
+		return "NewUser";
+	}
+}
+
+
